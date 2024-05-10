87eb0cc32768c3b0c423f744e20e72d6590df6ba
Jean Thomassen <jeanthomassen.04@gmail.com>
2024-05-10 10:36:30 -0300

    modificaciones varias

--- javascript/main.js
+++ javascript/main.js
@@ -5,9 +5,9 @@ toggle.addEventListener('change',(event) =>{
     let checked=event.target.checked;
     document.body.classList.toggle('dark_mode');
     if(checked == true){
-        label_toggle.innerHTML='<img src="images/assets/light-on.svg">';
+        label_toggle.innerHTML='<img src="images/assets/sun.svg">';
     }else if (checked == false){
-        label_toggle.innerHTML='<img src="images/assets/light-off.svg">';
+        label_toggle.innerHTML='<img src="images/assets/moon.svg">';
     }
 })
 
